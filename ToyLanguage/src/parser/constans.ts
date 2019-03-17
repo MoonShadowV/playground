6af59b5f5cb8c4c602f781750a76097eeb6eb87c
ShadowMoonV <834540085@qq.com>
2019-03-17 21:54:23 +0800

    dev:add constants and now we can use it instead of string.

---
+++ ToyLanguage/src/parser/constans.ts
@@ -0,0 +1,20 @@
+const NUMBER = "number";
+const STRING = "string";
+const BOOLEAN = "boolean";
+const VAR = "var";
+const ASSIGN = "assign";
+const LAMBDA = "lambda";
+const BINARY = "binary";
+const PROG = "prog";
+const CALL = "call";
+const IF = "if";
+const LET = "let";
+
+const KEYWORD = "keyword";
+const PUNCTUATION = "punctuation";
+const OPERATOR = "operator";
+
+export {
+    NUMBER,STRING,BOOLEAN,VAR,ASSIGN,LAMBDA,BINARY,PROG,CALL,IF,LET,KEYWORD,PUNCTUATION,OPERATOR
+
+}
